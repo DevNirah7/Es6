e5c4a9f27feec3942522e19d6673af491dc68cc5
Harin <89265161+DevNirah7@users.noreply.github.com>
2022-01-14 01:26:44 +0530

    feat Qstns 3,4

--- src/index.js
+++ src/index.js
@@ -26,3 +26,44 @@
 // ES6:
 // const multiply= (x,y) => x+y;
 // console.log(multiply(1,2))
+
+// 3.
+// Es5 syntax
+// var customer = {
+//   name: "Bhaalo"
+// };
+// var card = {
+//   amount: 20,
+//   product: "Aaalo",
+//   unitprice: 50
+// };
+// var message = "Hello " + customer.name + " wants to buy " + card.amount + " " + card.product +
+//     " for price of " + card.unitprice + " per piece";
+
+// ES6:
+
+// let customer=
+// {
+//   name: "Bhaloo"
+// };
+
+// let card=
+// {
+//  amount: 20,
+//  product: "Aaloo",
+//  unitprice:50
+// };
+// let message=`Hello ${customer.name} wants to buy ${card.amount} ${card.product} for price of ${card.unitprice} per piece`
+// console.log(message);
+
+// 4.
+// // Es5 syntax
+// var Neog = ["Tanvi", "Swap", "Tanay", "MA", "CA", "PA", "TA"],
+// CEO = Neog[0],
+// Mentor = Neog[2];
+
+// // // ES6:
+
+// const Neog = ["Tanvi", "Swap", "Tanay", "MA", "CA", "PA", "TA"];
+// const [CEO,Mentor]= Neog;
+// console.log(CEO,Mentor);
