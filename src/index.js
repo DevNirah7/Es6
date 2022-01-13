174c648450f4ff296d78229c79d6235af31d6be0
Harin <89265161+DevNirah7@users.noreply.github.com>
2022-01-13 22:47:31 +0530

    feat Qstn1,2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import "./styles.css";
+//
+// 1.
+// ES5 syntax
+// var aloo = 1;
+// if (aloo == 1) {
+//    var a = 2;
+//    console.log(a);
+// }
+// console.log(aloo);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// ES6:
+// const aloo=1;
+// if(aloo)
+// {
+//   const a=2;
+//   console.log(a)
+// }
+// console.log(aloo)
+
+// 2.
+// Es5 syntax
+// var multiply = function(x, y) {
+//   return x * y;
+// };
+
+// ES6:
+// const multiply= (x,y) => x+y;
+// console.log(multiply(1,2))
